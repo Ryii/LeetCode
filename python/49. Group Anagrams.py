526d3f2ac7b2bfb7cfecb8a96f6ab396ca3cd7a3
Ryan Sun <ryiijcsun@gmail.com>
2023-04-05 08:15:58 -0400

    49. Group Anagrams

---
+++ python/49. Group Anagrams.py
@@ -0,0 +1,23 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        hashMap = defaultdict(list)
+
+        for word in strs:
+            hashMap[tuple(sorted(word))].append(word)
+
+        return hashMap.values()
+
+
+
+        # hashMap = defaultdict(list)
+
+        # for word in strs:
+
+        #     wordDict = [0] * 26
+        #     for c in word:
+        #         wordDict[ord(c) - ord('a')] += 1
+
+        #     hashMap[tuple(wordDict)].append(word)
+
+        # return hashMap.values()
