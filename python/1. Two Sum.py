b42fe51309ae8e03c3aa36a6a7da9e00001ffadc
Ryan Sun <ryiijcsun@gmail.com>
2023-04-04 23:23:18 -0400

    1. Two Sum

---
+++ python/1. Two Sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashMap = defaultdict(list)
+
+        for i, n in enumerate(nums):
+            if target - n in hashMap:
+                return [hashMap[target - n], i]
+            else:
+                hashMap.update({n : i})
