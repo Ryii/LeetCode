bfd606a4f9dd6bd4b8a6765794e52da989384fcb
Ryan Sun <ryiijcsun@gmail.com>
2023-04-08 22:58:56 -0400

    128. Longest Consecutive Sequence

---
+++ python/128. Longest Consecutive Sequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        hashSet = set(nums)
+        sol = 0
+
+        for n in nums:
+            if (n - 1) not in hashSet:
+                length = 0
+                while n + length in hashSet:
+                    length += 1
+                sol = max(sol, length)
+
+        return sol
