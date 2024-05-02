8de447309e880baeb5e411827aa0582d92f137a4
Ryan Sun <ryiijcsun@gmail.com>
2024-05-01 20:53:56 -0400

    May 1, 2024

---
+++ 2024 Daily Problems/5. May/(1) 2000. Reverse Prefix of Word.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        if ch not in word:
+            return word
+        index = word.index(ch)
+
+        word = word[:index+1][::-1] + word[index+1:]
+
+        return word
