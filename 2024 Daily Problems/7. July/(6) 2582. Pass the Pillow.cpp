28a458ded7ef1425075ee9212e342df6d7a41eb0
Ryan Sun <ryiijcsun@gmail.com>
2024-07-05 21:06:30 -0400

    July 6, 2024

---
+++ 2024 Daily Problems/7. July/(6) 2582. Pass the Pillow.cpp
@@ -0,0 +1,9 @@
+class Solution
+{
+public:
+    int passThePillow(int n, int time)
+    {
+        time = time % (2 * (n - 1));
+        return time > n - 1 ? n - (time - (n - 1)) : time + 1;
+    }
+};
